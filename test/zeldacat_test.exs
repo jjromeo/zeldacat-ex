6a5a389e2150ef98d8d07e3f43490af039938466
Jerome Pratt <jerome.pratt@outlook.com>
2015-03-28 19:45:49 +0000

    refactored tests to seperate ones

--- test/zeldacat_test.exs
+++ test/zeldacat_test.exs
@@ -1,18 +1,32 @@
 defmodule ZeldacatTest do
   use ExUnit.Case
 
-  test "something with a health component can die" do
-    # Create entity, add health component, then kill it!
+  setup do
     {:ok, entity} = Entity.init()
     Entity.add_component(entity, HealthComponent, 100)
+  end
+
+  test "has health of 100 and is alive when initialized" do
     assert HealthComponent.get_hp(entity) == 100
     assert HealthComponent.alive?(entity) == true
+  end
+
+  test "it can lose health" do
+    Entity.add_component(entity, HealthComponent, 100)
     Entity.notify(entity, {:hit, 50})
     assert HealthComponent.get_hp(entity) == 50
+  end
+
+  test "it can be healed" do
+    Entity.notify(entity, {:hit, 50})
     Entity.notify(entity, {:heal, 25})
     assert HealthComponent.get_hp(entity) == 75
-    Entity.notify(entity, {:hit, 75})
+  end
+
+  test "it knows when it's dead" do
+    Entity.notify(entity, {:hit, 100})
     assert HealthComponent.get_hp(entity) == 0
     assert HealthComponent.alive?(entity) == false
   end
+
 end
