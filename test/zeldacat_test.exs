21692c3faa69b0dee5dc91e0301520b4d1cfb09a
Jerome Pratt <jerome.pratt@outlook.com>
2015-03-28 19:51:24 +0000

    made refactored tests work

--- test/zeldacat_test.exs
+++ test/zeldacat_test.exs
@@ -1,32 +1,34 @@
 defmodule ZeldacatTest do
   use ExUnit.Case
 
-  setup do
+  test "has health of 100 and is alive when initialized" do
     {:ok, entity} = Entity.init()
     Entity.add_component(entity, HealthComponent, 100)
-  end
-
-  test "has health of 100 and is alive when initialized" do
     assert HealthComponent.get_hp(entity) == 100
     assert HealthComponent.alive?(entity) == true
   end
 
   test "it can lose health" do
+    {:ok, entity} = Entity.init()
     Entity.add_component(entity, HealthComponent, 100)
     Entity.notify(entity, {:hit, 50})
     assert HealthComponent.get_hp(entity) == 50
   end
 
-  test "it can be healed" do
+  test "it can gain health" do
+    {:ok, entity} = Entity.init()
+    Entity.add_component(entity, HealthComponent, 100)
     Entity.notify(entity, {:hit, 50})
     Entity.notify(entity, {:heal, 25})
     assert HealthComponent.get_hp(entity) == 75
   end
 
-  test "it knows when it's dead" do
+  test "it knows it's dead at 0 health" do
+    {:ok, entity} = Entity.init()
+    Entity.add_component(entity, HealthComponent, 100)
+    assert HealthComponent.alive?(entity) == true
     Entity.notify(entity, {:hit, 100})
     assert HealthComponent.get_hp(entity) == 0
     assert HealthComponent.alive?(entity) == false
   end
-
 end
